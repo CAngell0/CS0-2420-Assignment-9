3d37a8748d9f4be3531851164fb86fc1741ff5bd
Carson Angell <carsonjesseangell@gmail.com>
2025-11-13 21:08:53 -0700

    Added timing experiment class stubs for q4

---
+++ app/src/main/java/timing/JavaContainsValueTimingExperiment.java
@@ -0,0 +1,23 @@
+package timing;
+
+import java.util.List;
+
+public class JavaContainsValueTimingExperiment extends TimingExperiment {
+
+    public JavaContainsValueTimingExperiment(String problemSizeName, List<Integer> problemSizes, int iterationCount) {
+        super(problemSizeName, problemSizes, iterationCount);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+}
