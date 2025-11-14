f47b3d60cdf36d4df30856efc6833d3a4cc66bb9
Carson Angell <carsonjesseangell@gmail.com>
2025-11-13 18:16:11 -0700

    Added timing experiment class stubs

---
+++ app/src/main/java/timing/StudentBadHashTimingExperiment.java
@@ -0,0 +1,22 @@
+package timing;
+
+import java.util.List;
+
+public class StudentBadHashTimingExperiment extends TimingExperiment {
+
+    public StudentBadHashTimingExperiment(String problemSizeName, List<Integer> problemSizes, int iterationCount) {
+        super(problemSizeName, problemSizes, iterationCount);
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+}
---
+++ app/src/main/java/timing/StudentMediumHashTimingExperiment.java
@@ -0,0 +1,22 @@
+package timing;
+
+import java.util.List;
+
+public class StudentMediumHashTimingExperiment extends TimingExperiment {
+
+    public StudentMediumHashTimingExperiment(String problemSizeName, List<Integer> problemSizes, int iterationCount) {
+        super(problemSizeName, problemSizes, iterationCount);
+    }
+
+    @Override
+    protected void setupExperiment(int problemSize) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setupExperiment'");
+    }
+
+    @Override
+    protected void runComputation() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runComputation'");
+    }
+}
